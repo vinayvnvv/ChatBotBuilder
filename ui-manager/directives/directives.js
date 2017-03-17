4316f5b307d3f8c9d5e0b1c7e425b773c858f839
vinayvnvv <tech.vinaybv@gmail.com>
2017-03-17 18:13:11 +0530

    new chages

--- ui-manager/directives/directives.js
+++ ui-manager/directives/directives.js
@@ -15,3 +15,49 @@ app.directive('ripple', ['$mdInkRipple', function($mdInkRipple){
 		}
 	};
 }]);
+
+
+
+app.directive('toggleOnScroll', ['$compile', function($compile){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		 // controller: function($scope, $element, $attrs, $transclude) {
+
+		 // },
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {			
+
+			var options = JSON.parse(iAttrs.toggleOnScroll);
+		 	$scope.target = iElm.find(options.elm);
+		 	console.log($scope.target)
+		 	$scope.marginD = $scope.target["0"].style.marginTop;			
+			
+
+			if(iElm[0].scrollTop <= options.top) {
+                    $scope.target.css("opacity", "0");
+                    $scope.target.css("margin-top", "-"+options.top)
+				}
+
+			iElm[0].onscroll = function() {
+				if(iElm[0].scrollTop > options.top) {
+                    $scope.target.css("opacity", "1");
+                    $scope.target.css("margin-top", $scope.marginD)
+				} else {
+                    $scope.target.css("opacity", "0");
+                    $scope.target.css("margin-top", "-"+options.top)
+				}
+			}
+			
+		}
+	};
+}]);
