e09e771a3085357513108a01cfc8abc3d9cae66f
Vinay <Vinay.bv@accionlabs.com>
2017-12-04 17:43:02 +0530

    changes

--- bot/build/script.js
+++ bot/build/script.js
@@ -5917,19 +5917,6 @@ alight.component('c-bot-loader', function (scope, element, env) {
 	    	onStart: function() {}
 	    };
 });
-alight.component('c-bot-mgs', function (scope, element, env) {
-	    return { 
-	    	templateUrl: "app/msgs/msgs.component.html",
-	    	onStart: function() {
-	    			scope.index = scope.index;
-	    			scope.data = scope.data;
-				    scope.name = scope.in;
-				    scope.click = function() {
-				    	scope.name = "Shannubhag"
-				    }
-	    	}
-	    };
-});
 alight.component('c-bot-sug', function (scope, element, env) {
 	    return { 
 	    	templateUrl: "app/suggestion/suggestion.component.html",
@@ -5940,6 +5927,19 @@ alight.component('c-bot-sug', function (scope, element, env) {
 	    			scope.onSelect = function(item) {
 	    				env.parentChangeDetector.scope.onSuggestionSelect(item);
 	    			}
+	    	}
+	    };
+});
+alight.component('c-bot-mgs', function (scope, element, env) {
+	    return { 
+	    	templateUrl: "app/msgs/msgs.component.html",
+	    	onStart: function() {
+	    			scope.index = scope.index;
+	    			scope.data = scope.data;
+				    scope.name = scope.in;
+				    scope.click = function() {
+				    	scope.name = "Shannubhag"
+				    }
 	    	}
 	    };
 });
